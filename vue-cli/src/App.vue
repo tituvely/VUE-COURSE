9b9cbff0c78b36794d6fab2b00d1f992c1694f77
Yookyung Kim <tituvely@gmail.com>
2021-12-04 23:12:39 +0900

    vue-cli

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header :message="str" @renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: { AppHeader },
+  data() {
+    return {
+      str: "Header",
+    };
+  },
+  methods: {
+    renewStr() {
+      this.str = "hi";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template lang="">
+    <header>
+        <h1>{{ message }}</h1>
+        <button @click="sendEvent">send</button>
+    </header>
+</template>
+<script>
+export default {
+  props: ["message"],
+  methods: {
+    sendEvent() {
+      this.$emit("renew");
+    },
+  },
+};
+</script>
+<style lang="">
+</style>
