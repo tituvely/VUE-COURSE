aa5c96ccaad35a9db411aa800da222433b984ec5
Yookyung Kim <tituvely@gmail.com>
2022-01-15 18:07:35 +0900

    store 모듈화

--- vue-intermediate/vue-todo/src/store/store.js
+++ vue-intermediate/vue-todo/src/store/store.js
@@ -1,32 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import * as getters from "./getters";
-import * as mutations from "./mutations";
+import todoApp from "./modules/todoApp";
 
 Vue.use(Vuex);
 
-const storage = {
-  fetch() {
-    const arr = [];
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length; i++) {
-        if (
-          localStorage.key(i) !== "loglevel:webpack-dev-server" &&
-          !localStorage.key(i).startsWith("ab.storage")
-        ) {
-          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-        }
-      }
-    }
-    return arr;
-  },
-};
-
 export const store = new Vuex.Store({
-  state: {
-    headerText: "TODO",
-    todoItems: storage.fetch(),
+  modules: {
+    todoApp,
   },
-  getters,
-  mutations,
 });
