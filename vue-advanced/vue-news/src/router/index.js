fd631ccb809ff2aba139111a45cbc9b05ed0545e
Yookyung Kim <tituvely@gmail.com>
2022-01-19 18:58:13 +0900

    axios를 이용한 api 호출

--- vue-advanced/vue-news/src/router/index.js
+++ vue-advanced/vue-news/src/router/index.js
@@ -3,8 +3,6 @@ import VueRouter from "vue-router";
 import NewsView from "../views/NewsView.vue";
 import AskView from "../views/AskView.vue";
 import JobsView from "../views/JobsView.vue";
-import ItemView from "../views/ItemView.vue";
-import UserView from "../views/UserView.vue";
 
 Vue.use(VueRouter);
 
@@ -26,14 +24,6 @@ export const router = new VueRouter({
     {
       path: "/jobs",
       component: JobsView,
-    },
-    {
-      path: "/item",
-      component: ItemView,
-    },
-    {
-      path: "/user",
-      component: UserView,
     },
   ],
 });
--- vue-advanced/vue-news/src/views/NewsView.vue
+++ vue-advanced/vue-news/src/views/NewsView.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users" :key="user">{{ user.title }}</div>
+    news
+  </div>
 </template>
 
 <script>
-export default {
+import axios from "axios";
 
-}
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => (this.users = response.data))
+      .catch(console.log);
+  },
+};
 </script>
 
 <style>
-
 </style>
