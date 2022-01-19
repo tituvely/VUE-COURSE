d5a1db81a17a75c73fc83d9e5c33bae69f38a4de
Yookyung Kim <tituvely@gmail.com>
2022-01-19 18:20:35 +0900

    redirect 와 router-link

--- vue-advanced/vue-news/src/App.vue
+++ vue-advanced/vue-news/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
+    <header></header>
+    <ToolBar></ToolBar>
+    <main></main>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from "./components/ToolBar.vue";
+
 export default {
   name: "App",
+  components: {
+    ToolBar,
+  },
 };
 </script>
 
