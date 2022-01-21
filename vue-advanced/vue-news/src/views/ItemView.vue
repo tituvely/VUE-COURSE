93768a621fd7793dc4735d13b4cae331f5b08865
Yookyung Kim <tituvely@gmail.com>
2022-01-22 00:44:51 +0900

    v-html

--- vue-advanced/vue-news/src/views/ItemView.vue
+++ vue-advanced/vue-news/src/views/ItemView.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <p>{{ item.title }}</p>
-    <p>{{ item.content }}</p>
+    <section>
+      <div class="user-container">
+        <div><i class="fas fa-user"></i></div>
+        <div class="user-description">
+          <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+          <div class="time">{{ item.time_ago }}</div>
+        </div>
+      </div>
+      <h2>{{ item.title }}</h2>
+    </section>
+    <section>
+      <div v-html="item.content"></div>
+    </section>
   </div>
 </template>
 
@@ -12,11 +23,25 @@ export default {
     ...mapGetters(["item"]),
   },
   created() {
-    const itemId = this.$router.params.id;
+    const itemId = this.$route.params.id;
     this.$store.dispatch("FETCH_ITEM", itemId);
   },
 };
 </script>
 
-<style>
+<style scoped>
+.user-container {
+  padding: 0.7rem;
+  display: flex;
+  align-items: center;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
 </style>
