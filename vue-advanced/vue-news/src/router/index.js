c64aad219924e0ea5a34a08451518b878510f6e7
Yookyung Kim <tituvely@gmail.com>
2022-01-22 16:53:45 +0900

    Mixin 적용 (HOC보다 컴포넌트 깊이가 얕음)

--- vue-advanced/vue-news/src/router/index.js
+++ vue-advanced/vue-news/src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-// import NewsView from "../views/NewsView.vue";
-// import AskView from "../views/AskView.vue";
-// import JobsView from "../views/JobsView.vue";
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
 import UserView from "../views/UserView.vue";
 import ItemView from "../views/ItemView.vue";
-import createListView from "../views/CreateListView";
 
 Vue.use(VueRouter);
 
@@ -19,17 +18,17 @@ export const router = new VueRouter({
     {
       path: "/news",
       name: "news",
-      component: createListView("NewsView"),
+      component: NewsView,
     },
     {
       path: "/ask",
       name: "ask",
-      component: createListView("AskView"),
+      component: AskView,
     },
     {
       path: "/jobs",
       name: "jobs",
-      component: createListView("JobsView"),
+      component: JobsView,
     },
     {
       path: "/user/:id",
--- vue-advanced/vue-news/src/views/NewsView.vue
+++ vue-advanced/vue-news/src/views/NewsView.vue
@@ -6,21 +6,13 @@
 
 <script>
 import ListItem from "../components/ListItem.vue";
-import bus from "../utils/bus.js";
+import ListMixin from "../mixins/ListMixin";
 
 export default {
   components: {
     ListItem,
   },
-  created() {
-    bus.$emit("start:spinner");
-    // 가상의 network 환경
-    setTimeout(() => {
-      this.$store.dispatch("FETCH_NEWS").then(() => {
-        bus.$emit("end:spinner");
-      });
-    }, 3000);
-  },
+  mixins: [ListMixin],
 };
 </script>
 
