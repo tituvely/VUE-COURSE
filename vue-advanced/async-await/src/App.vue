bf150cf3d1eda03f77a14ecb0a7e78804cf245ba
Yookyung Kim <tituvely@gmail.com>
2022-01-24 21:50:42 +0900

    axios 사용

---
+++ vue-advanced/async-await/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <button @click="loginUser">login</button>
+    <h1>List</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    loginUser() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users/1")
+        .then((response) => {
+          if (response.data.id === 1) {
+            axios
+              .get("https://jsonplaceholder.typicode.com/todos")
+              .then((response) => {
+                this.items = response.data;
+              })
+              .catch();
+          }
+        })
+        .catch((error) => console.log(error));
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
