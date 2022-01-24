944169b19c55ebd2cc6262b0f0513f5d142a5bc3
Yookyung Kim <tituvely@gmail.com>
2022-01-24 22:42:00 +0900

    라이브러리 컴포넌트화

---
+++ vue-charts/chart-lib/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Chart.js</h2>
+    <bar-chart></bar-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from "./components/BarChart.vue";
+export default {
+  components: { BarChart },
+};
+</script>
+
+<style>
+</style>
