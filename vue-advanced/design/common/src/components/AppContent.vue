8886d7fdeacf6a794a5f7e35600e0100cb0c3274
Yookyung Kim <tituvely@gmail.com>
2022-01-26 13:18:10 +0900

    component design patterns - common

---
+++ vue-advanced/design/common/src/components/AppContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+    <button @click="$emit('renew')">renew items</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
