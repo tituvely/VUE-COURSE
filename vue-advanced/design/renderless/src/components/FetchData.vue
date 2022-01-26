f8f60325b73fd2588efdb948e5f99ea6f56faa2b
Yookyung Kim <tituvely@gmail.com>
2022-01-26 15:54:20 +0900

    component design patterns - renderless

---
+++ vue-advanced/design/renderless/src/components/FetchData.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from "axios";
+
+export default {
+  props: ["url"],
+  data() {
+    return {
+      response: null,
+      loading: false,
+    };
+  },
+  created() {
+    axios
+      .get("this.url")
+      .then((response) => {
+        this.response = response.data;
+        this.loading = true;
+      })
+      .catch((error) => {
+        alert("[ERROR] fetching the data", error);
+        console.error();
+      });
+  },
+  render() {
+    return this.$scopedSlots.default({
+      response: this.response,
+      loading: this.loading,
+    });
+  },
+};
+</script>
