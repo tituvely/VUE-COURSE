fea07282ae46895098039612faae7ca2445010da
Yookyung Kim <tituvely@gmail.com>
2022-01-26 13:24:33 +0900

    component design patterns - slot

---
+++ vue-advanced/design/slots/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ul>
+      <item>아이템 1</item>
+      <item>아이템 2</item>
+      <item>아이템 3</item>
+      <item>아이템 4</item>
+      <item>아이템 5</item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Item from "./Item.vue";
+
+export default {
+  components: {
+    Item,
+  },
+};
+</script>
+
+<style>
+</style>
