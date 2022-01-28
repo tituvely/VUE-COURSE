aa22f710523fb21a7c179dbb2f75ed16968e8b46
Yookyung Kim <tituvely@gmail.com>
2022-01-28 16:50:48 +0900

    로그인 분기처리

--- vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue-endgame/vue-til/src/components/LoginForm.vue
@@ -56,8 +56,8 @@ export default {
           password: this.password,
         };
         const { data } = await loginUser(userData);
+        this.$store.commit('setUsername', data.user.username);
         this.$router.push('/main');
-        this.logMessage = `${data.user.username} 님 환영합니다`;
       } catch (error) {
         // 에러 핸들링할 코드
         this.logMessage = error.response.data;
