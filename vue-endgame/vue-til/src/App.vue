79d1081a75d714f228355ccb9a232e1504c78acf
Yookyung Kim <tituvely@gmail.com>
2022-01-28 13:44:26 +0900

    라우팅

--- vue-endgame/vue-til/src/App.vue
+++ vue-endgame/vue-til/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <header>
+      <router-link to="/login">login</router-link>
+      <router-link to="/signup">signup</router-link>
+    </header>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
