426caf0af3dfec44eb0323f485066a8b8f75a191
Yookyung Kim <tituvely@gmail.com>
2022-01-28 15:11:15 +0900

    로그인 기능

--- vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue-endgame/vue-til/src/components/LoginForm.vue
@@ -8,23 +8,18 @@
       <label for="password">pw: </label>
       <input id="password" type="text" v-model="password" />
     </div>
-    <div>
-      <label for="nickname">nickname: </label>
-      <input id="nickname" type="text" v-model="nickname" />
-    </div>
-    <button type="submit">회원 가입</button>
+    <button type="submit">로그인</button>
     <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
-import { registerUser } from '@/api/index';
+import { loginUser } from '@/api/index';
 export default {
   data() {
     return {
       username: '',
       password: '',
-      nickname: '',
       logMessage: '',
     };
   },
@@ -33,16 +28,14 @@ export default {
       const userData = {
         username: this.username,
         password: this.password,
-        nickname: this.nickname,
       };
-      const { data } = await registerUser(userData);
-      this.logMessage = `${data.username} 님이 가입되었습니다.`;
+      const { data } = await loginUser(userData);
+      this.logMessage = `${data.user.username} 님이 가입되었습니다.`;
       this.initForm();
     },
     initForm() {
       this.username = '';
       this.password = '';
-      this.nickname = '';
     },
   },
 };
