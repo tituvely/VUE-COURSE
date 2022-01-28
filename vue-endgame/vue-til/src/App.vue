c1708383a8d133d81ecb9b98c4d56f6770f0c86e
Yookyung Kim <tituvely@gmail.com>
2022-01-28 13:38:23 +0900

    라우팅

--- vue-endgame/vue-til/src/App.vue
+++ vue-endgame/vue-til/src/App.vue
@@ -3,20 +3,7 @@
 </template>
 
 <script>
-export default {
-  created() {
-    console.log('created');
-  },
-};
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
