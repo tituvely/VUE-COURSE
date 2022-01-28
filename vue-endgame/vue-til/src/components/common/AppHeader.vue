4c1f1928c314db755edb67a0054a97bcbc5046fa
Yookyung Kim <tituvely@gmail.com>
2022-01-28 16:55:05 +0900

    로그아웃 기능 구현

--- vue-endgame/vue-til/src/components/common/AppHeader.vue
+++ vue-endgame/vue-til/src/components/common/AppHeader.vue
@@ -8,6 +8,7 @@
     <div class="navigations">
       <template v-if="isUserLogin">
         <span>{{ $store.state.username }}</span>
+        <a @click="logoutUser">로그아웃</a>
       </template>
       <template v-else>
         <router-link to="/login">로그인</router-link>
@@ -22,6 +23,12 @@ export default {
   computed: {
     isUserLogin() {
       return this.$store.getters.isLogin;
+    },
+  },
+  methods: {
+    logoutUser() {
+      this.$store.commit('clearUsername');
+      this.$router.push('/login');
     },
   },
 };
