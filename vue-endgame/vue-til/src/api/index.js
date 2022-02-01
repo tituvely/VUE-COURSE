e35a0dad0603e4af8e8eecba4f3d03ec6d6f2713
Yookyung Kim <tituvely@gmail.com>
2022-02-01 15:33:01 +0900

    브라우저 저장소를 이용한 인증 값 관리

--- vue-endgame/vue-til/src/api/index.js
+++ vue-endgame/vue-til/src/api/index.js
@@ -1,14 +1,10 @@
 import axios from 'axios';
-import store from '@/store';
 import { setInterceptors } from './common/interceptors';
 
 // 엑시오스 초기화 함수
 function createInstance() {
   const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
-    headers: {
-      Authorization: store.state.token,
-    },
   });
   return setInterceptors(instance);
 }
