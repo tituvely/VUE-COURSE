fcef74d3cf4c1338b32b3c435e8897a7a826937a
Yookyung Kim <tituvely@gmail.com>
2022-02-02 21:13:38 +0900

    아아디 인풋 박스 테스트 코드 작성

--- vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -3,8 +3,15 @@ import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
   test('ID는 이메일 형식이어야 한다', () => {
-    const wrapper = shallowMount(LoginForm);
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test',
+        };
+      },
+    });
     const idInput = wrapper.find('#username');
-    console.log(idInput.html());
+    expect(idInput.element.value).toBe('test');
+    expect(wrapper.vm.isUsernameValid).toBeFalsy();
   });
 });
